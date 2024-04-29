af5ce3ba030369a55054a55d6b32aa3e07c9d019
Martin Zane <martin@zaneweb.com>
2024-04-29 15:49:31 +0200

    Add on method to User to be able to attach events

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import User from "./models/user";
 
 const user = new User({ name: "Mark", age: 35 });
+user.on("change", () => {});
 
-user.set({ name: "Milo", age: 28 });
-
-console.log(user.get("name"));
-console.log(user.get("age"));
+console.log(user);
--- src/models/user.ts
+++ src/models/user.ts
@@ -3,7 +3,15 @@ interface UserData {
   age?: number;
 }
 
+type Callback = () => void;
+
+interface Event {
+  [key: string]: Callback[];
+}
+
 class User {
+  private events: Event = {};
+
   constructor(private data: UserData) {}
 
   public get(propName: string): string | number {
@@ -12,6 +20,12 @@ class User {
 
   public set(data: UserData): void {
     Object.assign(this.data, data);
+  }
+
+  public on(event: string, callback: Callback): void {
+    const handlers = this.events[event] || [];
+    handlers.push(callback);
+    this.events[event] = handlers;
   }
 }
 
