10127c44a30222e0dedf206dc8f4524e16ce1ca5
Martin Zane <martin@zaneweb.com>
2024-05-01 08:39:14 +0200

    Move all logic and attributes from User to Model and extend Model on User

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,6 +1,10 @@
-import { AxiosPromise } from "axios";
+import { AxiosPromise, AxiosResponse } from "axios";
 
 type Callback = () => void;
+
+interface Data {
+  id?: number;
+}
 
 interface ModelAttributes<T> {
   getAll(): T;
@@ -18,6 +22,50 @@ interface ModelEvents {
   trigger(event: string): void;
 }
 
-class Model {}
+class Model<T extends Data> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: ModelEvents,
+    private sync: ModelSync<T>
+  ) {}
+
+  public get on() {
+    return this.events.on;
+  }
+  public get trigger() {
+    return this.events.trigger;
+  }
+  public get get() {
+    return this.attributes.get;
+  }
+
+  public set(data: T): void {
+    this.attributes.set(data);
+    this.events.trigger("change");
+  }
+
+  public fetch(): void {
+    const id = this.get("id");
+
+    if (typeof id !== "number") {
+      throw new Error("Cannot fetch without an id");
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  public save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger("save");
+      })
+      .catch(() => {
+        this.trigger("error");
+      });
+  }
+}
 
 export default Model;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,4 @@
-import { AxiosResponse } from "axios";
-import Attributes from "./attributes";
-import Eventing from "./eventing";
-import Sync from "./sync";
+import Model from "./model";
 
 export interface UserData {
   id?: number;
@@ -11,52 +8,6 @@ export interface UserData {
 
 const ROOT_URL = "http://localhost:3000/users";
 
-class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserData> = new Sync(ROOT_URL);
-  public attributes: Attributes<UserData>;
-
-  constructor(public data: UserData) {
-    this.attributes = new Attributes(data);
-  }
-
-  public get on() {
-    return this.events.on;
-  }
-  public get trigger() {
-    return this.events.trigger;
-  }
-  public get get() {
-    return this.attributes.get;
-  }
-
-  public set(data: UserData): void {
-    this.attributes.set(data);
-    this.events.trigger("change");
-  }
-
-  public fetch(): void {
-    const id = this.get("id");
-
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without an id");
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  public save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger("save");
-      })
-      .catch(() => {
-        this.trigger("error");
-      });
-  }
-}
+class User extends Model<UserData> {}
 
 export default User;
