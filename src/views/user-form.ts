ece32bef81f3ef7d302644a72675658e6cb9f2c1
Martin Zane <martin@zaneweb.com>
2024-05-01 09:51:46 +0200

    Add mapEvents and bindEvents methods to UserForm to be able to attach and trigger events on the generated HTML

--- src/views/user-form.ts
+++ src/views/user-form.ts
@@ -1,20 +1,46 @@
 class UserForm {
   constructor(public parent: Element) {}
 
+  mapEvents(): { [key: string]: () => void } {
+    return {
+      "click:button": () => {
+        console.log("Hello button!");
+      },
+      "mouseenter:h1": () => {
+        console.log("Hello header!");
+      },
+    };
+  }
+
   template(): string {
     return `
       <div>
         <h1>User form</h1>
         <input />
+        <button>Click me!</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const mappedEvents = this.mapEvents();
+
+    Object.keys(mappedEvents).forEach((key) => {
+      const [event, selector] = key.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(event, mappedEvents[key]);
+      });
+    });
   }
 
   render(): void {
     const template = document.createElement("template");
     template.innerHTML = this.template();
 
-    this.parent.append(template.content);
+    const { content } = template;
+    this.bindEvents(content);
+    this.parent.append(content);
   }
 }
 
