49e50aaa8fd0353d80fcb0747a6643f9630e99b7
Martin Zane <martin@zaneweb.com>
2024-05-01 11:03:56 +0200

    Add save method and extract methods in own functions

--- src/views/user-form.ts
+++ src/views/user-form.ts
@@ -4,31 +4,34 @@ import View from "./view";
 class UserForm extends View<User, UserData> {
   mapEvents(): { [key: string]: () => void } {
     return {
-      "click:.set-age": () => {
-        this.model.setRandomAge();
-      },
-
-      "click:.set-name": () => {
-        const input = this.parent.querySelector("input");
-        if (input) {
-          const name = input.value;
-          this.model.set({ name });
-        }
-      },
+      "click:.set-age": this.handleSetAge,
+      "click:.set-name": this.handleSetName,
+      "click:.save": this.handleSave,
     };
   }
+
+  handleSetAge = () => {
+    this.model.setRandomAge();
+  };
+  handleSetName = () => {
+    const input = this.parent.querySelector("input");
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
+
+  handleSave = () => {
+    this.model.save();
+  };
 
   template(): string {
     return `
       <div>
-        <h1>User form</h1>
-        <div>User name: ${this.model.get("name")}</div>
-        <div>User age: ${this.model.get("age")}</div>
-        <br />
-        <input />
+        <input placeholder="${this.model.get("name")}" />
         <button class="set-name">Update name</button>
-        <br />
         <button class="set-age">Set random age</button>
+        <button class="save">Save</button>
       </div>
     `;
   }
